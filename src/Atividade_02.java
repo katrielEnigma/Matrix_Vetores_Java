263e4dec9b85ace6b3dc98a1d1e687e06fbe3b14
naoeovarella <silviocordeirovarella@gmail.com>
2025-09-15 22:32:16 -0300

    gerar numero aleatorio

--- src/Atividade_02.java
+++ src/Atividade_02.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+
 public class Atividade_02 {
+    public static void main(String[] args) {
+
+        int[] quinzeNumerosAleatorios = new int[15];
+        Random random = new Random();
+
+        for (int i =0; i < quinzeNumerosAleatorios.length; i++){
+            quinzeNumerosAleatorios[i] = random.nextInt(100) + 1;
+        }
+
+        System.out.println("Números gerados:");
+        for (int i = 0; i < quinzeNumerosAleatorios.length; i++) {
+            System.out.print(quinzeNumerosAleatorios[i] + " ");
+        }
+
+
+    }
 }
